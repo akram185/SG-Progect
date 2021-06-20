8c6c150b338208dd1b1f44e93a4754c2a1caace0
waseeme <wmohammed@leaders6-12.org>
2021-06-20 15:36:37 -0400

    components and services created

--- client/client-list/src/app/app.module.ts
+++ client/client-list/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddClientComponent } from './add-client/add-client.component';
+import { ClientListComponent } from './client-list/client-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddClientComponent,
+    ClientListComponent
   ],
   imports: [
     BrowserModule,
